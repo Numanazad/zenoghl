9a694e4c9c6fb234ac5f68a71432dd79929306dd
Numanazad <namanajid54@gmail.com>
2023-03-17 01:39:56 +0530

    api key skeleton

--- integrate.js
+++ integrate.js
@@ -8,14 +8,23 @@ const port = 3000;
 app.use(bodyParser.json());
 
 const createAppointmentInZenoti = async (appointment) => {
+  const zenotiApiKey = process.env.ZENOTI_API_KEY;
   try {
-    const response = await axios.post('dummy url for zenoti appointments', {
+    const response = await axios.post('dummy url for zenoti appointments', 
+    {
       location: appointment.location,
       service: appointment.service,
       date: appointment.date,
       time: appointment.time,
       customer: appointment.customer,
-    });
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${zenotiApiKey}`,
+        'Content-Type': 'application/json',
+      },
+    }
+    );
     console.log(response.data);
   } catch (error) {
     console.error(error);
@@ -35,11 +44,19 @@ app.post('/ghl-webhook', async (req, res) => {
 });
 
 const setGHLWebhook = async () => {
+  const ghlApiKey = process.env.GHL_API_KEY;
   try {
-    const response = await axios.post('url for ghl webhooks', {
+    const response = await axios.post('url for ghl webhooks', 
+    {
       url: 'url for server side ghl appointment notification',
       event: 'appointment_created',
-    });
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${ghlApiKey}`,
+      },
+    }
+    );
     console.log(response.data);
   } catch (error) {
     console.error(error);
