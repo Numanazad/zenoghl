847319500218d2aee1218ed131bce006f15502c9
Numanazad <namanajid54@gmail.com>
2023-09-02 21:53:36 +0530

    updated the time

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const gettoken = async (userData) => {
         app_id: "6D82F90E-BEB7-4C62-ADE3-7E0FA0EB1E82",
         app_secret: "e24d8fcb53cf40999a9a93baa72e94624b6199a6775246c990985b8f979a3337",
         device_id: "c113476f-04e1-484c-b887-57414441cdcf", 
-        date: "2023-08-29T10:00:00"
+        date: "2023-09-03T10:00:00"
       }
       );
     return response.data;
